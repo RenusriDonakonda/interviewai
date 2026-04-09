5b8f3aec1580e7add5aefcbe12328764c15e28c6
RenusriDonakonda <renusridonakonda25@gmail.com>
2026-04-09 21:07:00 +0530

    Ensure avatar upload returns URL

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -46,6 +46,8 @@ const uploadAvatar = async (req, res, next) => {
       { new: true }
     ).select("-password");
 
+    if (!user) return res.status(404).json({ message: "User not found" });
+
     return res.status(200).json({ avatarUrl: user.avatarUrl });
   } catch (error) {
     return next(error);
